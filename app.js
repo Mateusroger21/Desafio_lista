f0a62e28f77c121c27412c9b41b4d225726945ed
carlos <rogersilva600@gmail.com>
2024-02-05 18:17:58 -0300

    Exercícios_básicos_de_lista.app.js.

---
+++ app.js
@@ -0,0 +1,7 @@
+let listaGenerica = [];
+let linguagemdeProgramacao = ['Javascript', 'c', 'c++', 'kotlin', 'Python'];
+linguagemdeProgramacao.push(['java', 'Ruby', 'GoLang']);
+let nomeLista = ['valdenio', 'mateus', 'cleyso'];
+console.log(nomeLista[0]);
+console.log(nomeLista[1]);
+console.log(nomeLista[2]);
